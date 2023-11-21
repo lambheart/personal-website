96ecde0ad6cbb0f468796d12f59b5173d71fdc59
Celeste Li <celeste_li@icloud.com>
2023-11-21 12:12:22 -0600

    connected to database and got posts to display on tech page

--- server.js
+++ server.js
@@ -1,6 +1,57 @@
 // Load Node modules
 var express = require('express');
 const ejs = require('ejs');
+var mysql = require('mysql2');
+var dotenv = require('dotenv').config();
+
+//list of posts
+var posts = [];
+
+const setPosts = (rows) => {
+    posts = rows;
+    console.log(posts);
+}
+
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    keepAliveInitialDelay: 10000, 
+    enableKeepAlive: true,
+    dateStrings: true
+})
+
+con.connect(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log("Connected to MySQL server");
+
+
+});
+
+let sql = "SELECT * FROM blogposts ORDER BY id DESC";
+
+con.query(sql, (error, rows, fields) => {
+    if (error) {
+        return console.error(error.message);
+    }
+
+    setPosts(rows);
+});
+
+con.end(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log("Close connection to MySQL server");
+});
+
+
+
 // Initialise Express
 var app = express();
 // Render static files
@@ -28,5 +79,9 @@ app.get('/art', function (req, res) {
 
 // tech route
 app.get('/tech', function (req, res) {
-    res.render('pages/tech');
+    res.render('pages/tech', {
+        posts: posts
+    });
 });
+
+//error handler
